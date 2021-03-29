b0fda199dcb79f93af80ce67d40d9b076dfb9135
denn81 <67226344+denn81@users.noreply.github.com>
2021-03-29 09:54:21 +0400

    Update generate_poem.py

--- generate_poem.py
+++ generate_poem.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     parser.add_argument('--n-syllables', type=int, default=8)
     parser.add_argument('--sampling-k', type=int, default=50000)
     parser.add_argument('--beam-width', type=int, default=None)
-    parser.add_argument('--temperature', type=float, default=1.0)
+    parser.add_argument('--temperature', type=float, default=5.0)
     parser.add_argument('--last-text', type=str, default="")
-    parser.add_argument('--count', type=int, default=100)
+    parser.add_argument('--count', type=int, default=20)
     args = parser.parse_args()
 
     kwargs = vars(args)
